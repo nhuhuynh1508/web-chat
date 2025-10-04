015cc0522cb65553a2c4a3032751cc856269154f
Tsumiki <tamnhu03@gmail.com>
2025-10-04 15:15:47 +0700

    Create useConversation to detect active ones
    - using createContext

---
+++ hooks/useConversation.tsx
@@ -0,0 +1,32 @@
+// this hook detects if a conversation is active by checking the conversationID in the URL parameter
+"use client";
+import React, { useState, createContext, use } from "react";
+
+type ConversationContextType = {
+    isActive: boolean;
+    conversationID: string;
+    setConversationID: (id: string) => void;
+}
+
+const ConversationContext = createContext<ConversationContextType | undefined>(undefined);
+
+export const ConversationProvider = ({children}: {children: React.ReactNode}) => {
+    const [conversationID, setConversationID] = useState<string>("");
+    // !! to convert to boolean
+    const isActive = !!conversationID;
+    
+    return (
+        <ConversationContext.Provider value={{conversationID, setConversationID, isActive}}>
+            {children}
+        </ConversationContext.Provider>
+    );
+}
+
+export const useConversation = () => {
+    const context = use(ConversationContext);
+    if (!context) {
+        throw new Error("useConversation must be used within a ConversationProvider");
+    }
+    
+    return context;
+}
